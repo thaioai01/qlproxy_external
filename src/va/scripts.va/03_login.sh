874dad3818992e24a2128aa04c919c31977629cb
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-09-15 15:03:52 +0200

    fixed error in Ubuntu banner generation

--- src/va/scripts.va/03_login.sh
+++ src/va/scripts.va/03_login.sh
@@ -12,38 +12,38 @@ sudo echo root:Passw0rd | sudo chpasswd
 # now we allow root login for ssh
 sed -i "s/PermitRootLogin without-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
 
-# now setup /etc/issue login banner (on Ubuntu only)
-if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
-	echo "The script works only on Ubuntu 14"
-	exit 1
-fi
-
-# copy the login script to qlproxy directory
-cp vm_login.sh /opt/qlproxy/bin/
-
-# create a system wide interface up script
-cat > /etc/network/if-up.d/vm_login_update <<EOFTEXT
-#!/bin/sh
-if [ "\$METHOD" = loopback ]; then
-    exit 0
-fi
-
-if [ "\$MODE" != start ]; then
-    exit 0
-fi
-
-/bin/bash /opt/qlproxy/bin/vm_login.sh
-EOFTEXT
-
-# make it executable
-chmod +x /etc/network/if-up.d/vm_login_update
-
 # install vm tools (only if vmware is detected)
 dmesg | grep -i "hypervisor detected: vmware" > /dev/null
 if [ $? -eq 0 ]; then
     echo "Detected VMware, installing open-vm-tools..."
-    apt-get update > /dev/null
-    apt-get install -y open-vm-tools
+    if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
+        yum -y install open-vm-tools
+        systemctl enable vmtoolsd.service && systemctl start vmtoolsd.service
+    else
+        apt-get update > /dev/null
+        apt-get install -y open-vm-tools
+    fi
+fi
+
+# copy the /etc/issue creation script to qlproxy folder
+cp va_issue.sh /opt/qlproxy/bin/
+
+# now setup /etc/issue login banner
+if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]
+then
+    # we are on centos 7 - create a system wide interface up script
+    echo "On CENTOS NOT IMPL"
+    #    ???
+    #cp centos7/issue_update /etc/network/if-up.d/issue_update
+
+    # and make it executable
+    #chmod +x /etc/network/if-up.d/issue_update
+else
+    # we are on ubuntu - create a system wide interface up script
+    cp ubuntu14/issue_update /etc/network/if-up.d/issue_update
+
+    # and make it executable
+    chmod +x /etc/network/if-up.d/issue_update
 fi
 
 # disable the user
---
+++ src/va/scripts.va/va_issue.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+IPADDR=`/sbin/ifconfig | grep "inet addr" | grep -v "127.0.0.1" | awk '{ print $2 }' | awk -F: '{ print $2 }'`
+CPUNUM=`cat /proc/cpuinfo | grep processor | wc -l`
+RAMNFO=`free -mh | grep Mem: | awk {'print $2, "total,", $4, "free" '}`
+DISKSZ=`df -h | grep "/$" | awk {'print $2, "total,", $4, "free" '}`
+VA_VER=`/opt/qlproxy/bin/ldap --version`
+SQUID_VER='3.5.19'
+
+# some string manupulation magic
+OSINFO_TMP1=`cat /etc/os-release | grep VERSION=`
+OSINFO_NAME=${OSINFO_TMP1#VERSION=}
+OSINFO_NAME=${OSINFO_NAME#\"}
+OSINFO_NAME=${OSINFO_NAME%\"}
+
+OSINFO_TMP2=`cat /etc/os-release | grep ^NAME=`
+OSINFO_DIST=${OSINFO_TMP2#NAME=}
+OSINFO_DIST=${OSINFO_DIST#\"}
+OSINFO_DIST=${OSINFO_DIST%\"}
+
+echo "Welcome to Diladele Web Safety for Squid Proxy Web Filtering Appliance!"
+echo 
+echo "Operating System    $OSINFO_DIST, $OSINFO_NAME"
+echo "System Kernel       \\r"
+echo "System Arch         \\m"
+echo "RAM Available       $RAMNFO"
+echo "CPU Count           $CPUNUM"
+echo "Hard Disk Size      $DISKSZ"
+echo
+echo "Appliance Version   $VA_VER"
+echo "Squid Version       $SQUID_VER"
+echo "Default Username    root"
+echo "Default Password    Passw0rd"
+echo "Installation Dir    /opt/qlproxy"
+echo 
+echo "To use this Virtual Appliance - adjust your browser proxy settings to point"
+echo "to the IP address or domain name of this box $IPADDR, port 3128 and browse the web."
+echo 
+echo "Full featured Administrators Web Console is available at http://$IPADDR:80"
+echo 
+echo 
