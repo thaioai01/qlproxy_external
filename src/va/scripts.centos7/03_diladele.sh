560c6c7d0bab114f93a0296330e5f4bb23d99454
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-10-31 12:31:48 +0100

    next 4.8 build

--- src/va/scripts.centos7/03_diladele.sh
+++ src/va/scripts.centos7/03_diladele.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="4.8.0"
-MINOR="859E"
+MINOR="6002"
 
 # get latest qlproxy
 curl -O http://packages.diladele.com/qlproxy/$MAJOR.$MINOR/amd64/release/centos7/qlproxy-${MAJOR}-${MINOR}.x86_64.rpm
--- src/va/scripts.debian8/06_diladele.sh
+++ src/va/scripts.debian8/06_diladele.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="4.8.0"
-MINOR="859E"
+MINOR="6002"
 ARCH="amd64"
 
 # get latest qlproxy
