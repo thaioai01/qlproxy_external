80e53851fafebbaa9cc1688f7618570d46ad3a69
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-11-05 20:48:30 +0100

    new 4.8 build

--- src/va/scripts.suse/03_diladele.sh
+++ src/va/scripts.suse/03_diladele.sh
@@ -7,10 +7,10 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # get qlproxy
-wget http://packages.diladele.com/qlproxy/4.8.0.FEAC/amd64/release/suse13/qlproxy-4.8.0-FEAC.x86_64.rpm
+wget http://packages.diladele.com/qlproxy/4.8.0.9ABC/amd64/release/suse13/qlproxy-4.8.0-9ABC.x86_64.rpm
 
 # install it
-rpm -i qlproxy-4.8.0-FEAC.x86_64.rpm
+rpm -i qlproxy-4.8.0-9ABC.x86_64.rpm
 
 # restart apache
 systemctl restart apache2
