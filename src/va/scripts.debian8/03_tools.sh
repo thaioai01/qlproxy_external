e9985ce6a1dde800426045563537e1b2d53c540d
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-11-23 16:56:21 +0100

    compiling 3.5.22 without ecap

--- src/va/scripts.debian8/03_tools.sh
+++ src/va/scripts.debian8/03_tools.sh
@@ -7,14 +7,29 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # install build tools
-apt-get -y install devscripts build-essential fakeroot libssl-dev
+apt-get -y install devscripts build-essential fakeroot debhelper dh-autoreconf cdbs
+
+# install build dependences for squid
+# apt-get -y build-dep libecap
+apt-get -y build-dep squid3
+
+# uninstall libecap2 as we will be using the libecap3
+# apt-get -y --purge remove libecap2-dev libecap2
+
+# install additional packages for new squid
+#apt-get -y install nettle-dev libgnutls28-dev libssl-dev libdbi-perl
+#	libldap2-dev  libpam0g-dev libdb-dev libsasl2-dev libcppunit-dev
+#	libkrb5-dev comerr-dev libcap2-dev libexpat1-dev libxml2-dev
+#	libnetfilter-conntrack-dev
+
+
 
 # fetch the source for the package to re-build
-apt-get source squid3
+# apt-get source squid3
 
 # reset the owner
-CURRENT_USER=`logname`
-chown -R $CURRENT_USER:$CURRENT_USER .
+# CURRENT_USER=`logname`
+# chown -R $CURRENT_USER:$CURRENT_USER .
 
 # fetch dependent packages for the build
-apt-get -y build-dep squid3
+# apt-get -y build-dep squid3
